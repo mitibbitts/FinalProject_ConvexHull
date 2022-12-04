33febddff61dc3d02dfe6fa5b5c36a332dee2c66
Matthew Tibbitts <mitibbitts@uri.edu>
2022-12-03 21:32:23 -0500

    add reading from files
    file info:
    xCoordinate yCoordinate, there CAN be more values but will not read extra values

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
+#include <vector>
+#include <list>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include"Hullgenerator.cpp"
+#include "node.h"
 
+//read input file and transform into (vector? hash? linkedList?)
+void readFile(std::vector<Node>& pointList, std::string& fileName) {
 
-int main(){
-	HullGenerator hull;
-	hull.generateOutput();
-	
+    std::ifstream inFile(fileName);
+    std::string line;
 
-return 0;
+    while (std::getline(inFile, line)) {
+        std::istringstream stream(line);
+        std::vector<int> temp;
+        int val;
+        while (stream >> val) {
+            temp.push_back(val);
+        }
+        //std::cout << temp[0] << ' ' << temp[1] << std::endl;
+        Node node(temp[0], temp[1]);
+        //std::cout << "size 2" << std::endl;
+        pointList.push_back(node);
+    }
 }
+
+//output file as a .dot
+
+
+int main() {
+
+    std::string file;
+    std::cin >> file;
+    std::vector<Node> points;
+    readFile(points, file);
+
+    HullGenerator hull;
+    hull.generateOutput();
+
+
+	return 0;
+}
+
+
