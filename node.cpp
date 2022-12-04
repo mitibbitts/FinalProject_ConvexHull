6ac7669b71628c6717097656952859832064a66a
Matthew Tibbitts <mitibbitts@uri.edu>
2022-12-04 01:44:40 -0500

    Update node.cpp

--- node.cpp
+++ node.cpp
@@ -13,20 +13,8 @@ Node::Node(int x, int y) {
     this->next == nullptr;
 }
 
-Node::Node(int x, int y, int type) {
-    this->x = x;
-    this->y = y;
-    this->type = type;
-    this->next == nullptr;
-}
-
-void Node::setEdge() {
-    this->type = 1;
-}
-
-//Should be redundant but just in case
-void Node::setCenter() {
-    this->type = 0;
+void Node::setEdge(int edge) {
+    this->path = edge;
 }
 
 int Node::getX() {
@@ -37,6 +25,10 @@ int Node::getY() {
     return this->y;
 }
 
-int Node::getType() {
-    return this->type;
+int Node::getPath() {
+    return this->path;
+}
+
+Node Node::getNext() {
+    return this->next;
 }
